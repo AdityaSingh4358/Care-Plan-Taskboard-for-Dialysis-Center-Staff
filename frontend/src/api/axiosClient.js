e4a72d96e99c5fd2ac7e0541beadf14551e31294
AdityaSingh4358 <adityamohansingh7252@gmail.com>
2026-04-01 19:29:03 +0530

    updated backend api url

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://care-plan-taskboard-for-dialysis-center.onrender.com',
 })
 
 export default axiosClient
